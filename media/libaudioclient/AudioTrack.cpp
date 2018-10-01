99cb0c9c06a288f61e7027fc78cd6a9934ed71e0
Ippei Murata <ippei.x.murata@sony.com>
2018-10-01 17:37:50 +0900

    Set default value to mAttributes in AudioTrack
    mAttributes in AudioTrack can not be set in AudioTrack::set()
    when pAttributes is NULL.
    AudioTrack::getPosition() sometimes returns wrong value
    since mAttributes is undefined value.
    This change sets default values to mAttributes.
    
    Bug: 123607740
    Test: manual
    Change-Id: Ie590c17efc466cc89cbd7e9f89fc3ebeed98eace
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

--- media/libaudioclient/AudioTrack.cpp
+++ media/libaudioclient/AudioTrack.cpp
@@ -280,6 +280,11 @@ AudioTrack::AudioTrack(
       mPreviousSchedulingGroup(SP_DEFAULT),
       mPausedPosition(0)
 {
+    mAttributes.content_type = AUDIO_CONTENT_TYPE_UNKNOWN;
+    mAttributes.usage = AUDIO_USAGE_UNKNOWN;
+    mAttributes.flags = 0x0;
+    strcpy(mAttributes.tags, "");
+
     (void)set(streamType, sampleRate, format, channelMask,
             frameCount, flags, cbf, user, notificationFrames,
             0 /*sharedBuffer*/, false /*threadCanCallJava*/, sessionId, transferType,
@@ -312,6 +317,11 @@ AudioTrack::AudioTrack(
       mSelectedDeviceId(AUDIO_PORT_HANDLE_NONE),
       mTrackOffloaded(false)
 {
+    mAttributes.content_type = AUDIO_CONTENT_TYPE_UNKNOWN;
+    mAttributes.usage = AUDIO_USAGE_UNKNOWN;
+    mAttributes.flags = 0x0;
+    strcpy(mAttributes.tags, "");
+
     (void)set(streamType, sampleRate, format, channelMask,
             0 /*frameCount*/, flags, cbf, user, notificationFrames,
             sharedBuffer, false /*threadCanCallJava*/, sessionId, transferType, offloadInfo,
