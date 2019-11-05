d7a9db807a907434d416b3450060e95acad23df6
Marko Man <darkobas@gmail.com>
2019-11-05 19:03:47 +0100

    cameraservice: move client_package_name to data/misc
    Change-Id: I0d244b81625fd9514000bea313a28ad60321333f

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -2541,7 +2541,7 @@ status_t CameraService::BasicClient::startCameraOps() {
     sCameraService->mUidPolicy->registerMonitorUid(mClientUid);
 
 #ifdef TARGET_NEEDS_CLIENT_INFO
-    std::ofstream cpf("/data/vendor/xtended/client_package_name");
+    std::ofstream cpf("/data/misc/xtended/client_package_name");
     std::string cpn = String8(mClientPackageName).string();
     cpf << cpn;
 #endif
