aeaf133cce3f8f375a299a6819f0952268713f6f
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-11-09 19:49:41 +0200

    libcameraservice: add TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB
    Directly set camera package name by using OnePlusCameraProvider service.
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I57f150ccdcec476edda342ee1eed11bde20e427f
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -81,6 +81,10 @@
 #include "utils/CameraThreadState.h"
 #include "utils/CameraServiceProxyWrapper.h"
 
+#ifdef CAMERA_NEEDS_CLIENT_INFO_LIB
+#include <vendor/oneplus/hardware/camera/1.0/IOnePlusCameraProvider.h>
+#endif
+
 namespace {
     const char* kPermissionServiceName = "permission";
 }; // namespace anonymous
@@ -100,6 +104,9 @@ using hardware::camera2::ICameraInjectionCallback;
 using hardware::camera2::ICameraInjectionSession;
 using hardware::camera2::utils::CameraIdAndSessionConfiguration;
 using hardware::camera2::utils::ConcurrentCameraIdCombination;
+#ifdef CAMERA_NEEDS_CLIENT_INFO_LIB
+using ::vendor::oneplus::hardware::camera::V1_0::IOnePlusCameraProvider;
+#endif
 
 // ----------------------------------------------------------------------------
 // Logging support -- this is for debugging only
@@ -139,6 +146,10 @@ static const String16
 const char *sFileName = "lastOpenSessionDumpFile";
 static constexpr int32_t kVendorClientScore = resource_policy::PERCEPTIBLE_APP_ADJ;
 static constexpr int32_t kVendorClientState = ActivityManager::PROCESS_STATE_PERSISTENT_UI;
+
+#ifdef CAMERA_NEEDS_CLIENT_INFO_LIB
+static const sp<IOnePlusCameraProvider> gVendorCameraProviderService = IOnePlusCameraProvider::getService();
+#endif
 
 const String8 CameraService::kOfflineDevice("offline-");
 
@@ -3203,7 +3214,9 @@ status_t CameraService::BasicClient::startCameraOps() {
         std::string cpn = String8(mClientPackageName).string();
         cpf << cpn;
     #endif
-
+    #ifdef CAMERA_NEEDS_CLIENT_INFO_LIB
+        gVendorCameraProviderService->setPackageName(String8(mClientPackageName).string());
+    #endif
     return OK;
 }
 
